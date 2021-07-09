bfd76297fba48e9205eb2efbcc8cec2d1ce6a343
hongyunfei <34985241+hongyunfei@users.noreply.github.com>
2021-07-09 20:42:26 +0800

    Update jqc_exchange3.js

--- jqc_exchange3.js
+++ jqc_exchange3.js
@@ -49,7 +49,6 @@ let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
     return;
   }
-  await wait(50000)
   for (let j = 0; j < randomCount; ++j)
     for (let i = 0; i < cookiesArr.length; i++) {
       if (cookiesArr[i]) {
