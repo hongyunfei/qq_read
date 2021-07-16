56d32011f79f1a6c805729e80e390acf8013cdaf
jeantoe3 <80583261+jeantoe3@users.noreply.github.com>
2021-07-16 21:44:05 +0800

    Add files via upload

--- jx_cfdhb.js
+++ jx_cfdhb.js
@@ -54,7 +54,7 @@ function cashOut(ac) {
 
 function taskUrl(function_path, body, ck) {
     return {
-        url: `${JD_API_HOST}jxbfd/${function_path}?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=${Date.now()}&ptag=7155.9.47&dwType=3&dwLvl=10&${body}&_stk=_cfd_t%2CbizCode%2CddwPaperMoney%2CdwEnv%2CdwLvl%2CdwType%2Cptag%2Csource%2CstrPoolName%2CstrZone&_ste=1&h5st=20210714194505287%3B0008312373818162%3B10032%3Btk01wef211d4ea8nVnBKbkI2YWMyLsdfUxEgOgzh%2BGXmysR3jrvoYsR5UiQmQXKDaskgnKcEhmAqe%2BhVC5n3FVHFTTjE%3B30385d4f25fc9cb5170507d9d9648abe6d396d055b3f891d622eb37671cf288c&_=${Date.now()}&sceneval=2&g_login_type=1&g_ty=ls`,
+        url: `${JD_API_HOST}jxbfd/${function_path}?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=${Date.now()}&ptag=7155.9.47&dwType=2&dwLvl=10&${body}&_stk=_cfd_t%2CbizCode%2CdwEnv%2CdwType%2Cptag%2Csource%2CstrZone&_ste=1&h5st=20210716213109967%3B5179674085058161%3B10032%3Btk01w90711b4aa8ndyt3TWozTXQxF4AL0IW76j8ecJy%2F8cS0Z2s0VLF0KPcAm3B%2FmGsyutNcIUiVn3AQGoRDAk1QBqCo%3B70f6e0f27ba19790bd58b13781896d763d72b11b2d81f093fd044e1170bfcdd0&_=${Date.now()}&sceneval=2&g_login_type=1&g_ty=ls`,
         headers: {
             Cookie: ck,
             Accept: "*/*",
